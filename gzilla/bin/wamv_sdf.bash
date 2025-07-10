406185f566ca4e821421e5ad4a9395de4732c94a
Paul Szenher <pszenher@stevens.edu>
2025-07-10 13:51:25 -0400

    Purge unused `gzilla` scripts and config

--- gzilla/bin/wamv_sdf.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-xacro "$(ros2 pkg prefix --share virelex)/urdf/wamv_gazebo.urdf.xacro" \
-      "namespace:=wamv27" \
-      "vrx_sensors_enabled:=true" \
-    | /ws/src/virelex/bin/urdf2sdf
